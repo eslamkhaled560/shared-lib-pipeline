4336611b584dc6bab102e7766432b3a20923b62a
eslamkhaled560 <54172897+eslamkhaled560@users.noreply.github.com>
2023-06-23 19:37:30 +0300

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def buildImage() {
+def call() {
     echo "Building docker image..."
     sh "docker build -t islamdevops/pvt-repo:${BRANCH_NAME} ."
     echo "Building is successful"
