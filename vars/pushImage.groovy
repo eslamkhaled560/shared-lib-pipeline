a245f600aa63ba80f4a6050e1c406e99577fc2c6
eslamkhaled560 <54172897+eslamkhaled560@users.noreply.github.com>
2023-06-23 19:44:06 +0300

    Update pushImage.groovy

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -3,6 +3,7 @@
 def call() {
     echo "Pushing docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'USER', passwordVariable: 'PWD')]) {
-    sh "echo $PASS | docker login -u $USER --password-stdin" }
+        sh "echo $PASS | docker login -u $USER --password-stdin" 
+    }
     sh "docker push islamdevops/pvt-repo:${BRANCH_NAME}"
     echo "Pushing is successful"
